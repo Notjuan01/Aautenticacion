c7629f814de1b5d118874ffc9aa24f53b87ab28c
notjuan01 <mjuanpablo523@gmail.com>
2024-11-05 19:39:45 -0500

    creacion de la pagina de ejemplo

---
+++ Pages/Estudiantes/Estudiantes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Autenticacion.Pages.Estudiantes
+{
+    public class EstudiantesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- obj/Debug/net8.0/Autenticacion.AssemblyInfo.cs
+++ obj/Debug/net8.0/Autenticacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Autenticacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ead261d857aae9fd5dfef65cd3d5c9982500bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Autenticacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Autenticacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
